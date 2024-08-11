385b4b5c92991cbae75ed808aac59f12fa68b97b
Koval89 <alexandrgalaxy4@gmail.com>
2024-08-11 19:15:23 +0300

    HW3

--- HW 3/main.js
+++ HW 3/main.js
@@ -246,69 +246,71 @@
 
 // 8. вивести масив в зворотньому порядку.
 
-// let rev = arr.reverse()
-// console.log(rev)
+// for (let i = arr.length - 1; i >= 0; i--) {
+//     console.log(arr[i])
+//
+// }
 
 
 // 9. всі попередні завдання (окрім 8), але в зворотньому циклі (с заду на перед)
 
 // let arr = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18]
 
-// let rev = arr.reverse()
-// let i = 0
-// while (i < rev.length) {
-//     console.log(rev[i])
-//     i++
+
+// let i = arr.length - 1
+// while (i >= 0) {
+//     console.log(arr [i])
+//     i--
 // }
 
-// let rev = arr.reverse()
-// for (let j = 0; j < arr.length; j++) {
-//     console.log(rev[j])
-// }
-
-// let rev = arr.reverse()
-// let i = 0
-// while (i < rev.length) {
-//     if (rev[i] % 2 !== 0) {
-//         console.log(rev[i])
-//
-//     }
-//     i++
-// }
-
-// let rev = arr.reverse()
-// for (let i = 0; i < arr.length; i++) {
-//     if (rev[i] % 2 !== 0) {
-//         console.log(rev[i])
-//     }
+// for (let i = arr.length - 1; i >= 0; i--) {
+//     console.log(arr[i])
 //
 // }
 
-// let rev = arr.reverse()
-// let i = 0
-// while (i < arr.length) {
-//     if (rev[i] % 2 === 0) {
-//         console.log(rev[i])
+
+// let i = arr.length -1
+// while (i >=0) {
+//     if (arr[i] % 2 !== 0) {
+//         console.log(arr[i])
+//
 //     }
-//     i++
+//     i--
 // }
 
-// let rev = arr.reverse()
-// for (let i = 0; i < arr.length; i++) {
-//     if (rev[i] % 2 === 0) {
-//         console.log(rev[i])
+
+// for (let i = arr.length - 1; i >= 0; i--) {
+//     if (arr[i] % 2 !== 0) {
+//         console.log(arr[i])
 //     }
 //
 // }
 
-// let rev = arr.reverse()
-// for (let i = 0; i < arr.length; i++) {
-//     if (rev[i] % 3 === 0){
-//         rev[i]= "okten"
+
+// let i = arr.length - 1
+// while (i >= 0) {
+//     if (arr[i] % 2 === 0) {
+//         console.log(arr[i])
+//     }
+//     i--
+// }
+
+
+// for (let i = arr.length - 1; i >= 0; i--) {
+//     if (arr[i] % 2 === 0) {
+//         console.log(arr[i])
 //     }
 //
 // }
-// console.log(rev)
+
+
+// for (let i = arr.length - 1; i >= 0; i--) {
+//     if (arr[i] % 3 === 0) {
+//         arr[i] = "okten"
+//
+//     }
+//     console.log(arr[i])
+// }
 
 
 // - Створити масив з 10 числових елементів. Вивести в консоль всі його елементи в циклі.
@@ -375,11 +377,98 @@
 
 
 // - Створити порожній масив. Наповнити його 10 елементами (різними за типами) через звернення до конкретних індексів. Вивести в консоль всі його елементи в циклі.
+
+// let str = ['ford', 'acura', 'lada', 'geely', 'volvo', 'volkswagen', 'toyota', 'hyundai', 'haval', 'opel']
+// let num = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18]
+// let bool = [true,false]
+// let arr = []
+// arr[0]=str[2]
+// arr[1]=bool[0]
+// arr[2]=num[3]
+// arr[3]=str[4]
+// arr[4]=num[5]
+// arr[5]=bool[1]
+// arr[6]=num[9]
+// arr[7]=str[8]
+// arr[8]=num[0]
+// arr[9]=bool[0]
+//
+//
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i])
+//
+// }
+
+
 // - Створити цикл for на 10  ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
+
+// for (let i = 0; i < 10; i++) {
+//     console.log('item', [i])
+//
+// }
+//
+// for (let j = 0; j < 10; j++) {
+//     // document.write(`<div>item - ${j}</div>`)
+//
+//     document.write(`<ul>`)
+//     document.write(`<li>item - ${j}</li>`)
+//     document.write(`</ul>`)
+//
+// }
+
 // - Створити цикл for на 100 ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
+
+// for (let i = 0; i < 100; i++) {
+//     console.log('item', [i])
+// }
+//
+// for (let i = 0; i < 100; i++) {
+//    document.write(`<div>item - ${i}</div>`)
+//
+// }
+
 // - Створити цикл for на 100 ітерацій з кроком 2. Вивести поточний номер кроку через console.log та document.write
+
+// for (let i = 0; i < 100; i=i+2) {
+//     console.log('item',[i])
+// }
+
+// for (let i = 0; i < 100; i=i+2) {
+//     document.write(`<div>item - ${i}</div>`)
+//
+// }
+
+
 // - Створити цикл for на 100 ітерацій. Вивести тільки парні кроки. через console.log + document.write
+
+// for (let i = 0; i < 100; i++) {
+//     if ([i] % 2 === 0) {
+//         console.log( [i])
+//     }
+// }
+
+// for (let i = 0; i < 100; i++) {
+//     if ([i] % 2 === 0) {
+//         document.write(`<div>${i}</div>`)
+//     }
+//
+// }
+
+
 // - Створити цикл for на 100 ітерацій. Вивести тільки непарні кроки. через console.log + document.write
+
+// for (let i = 0; i < 100; i++) {
+//     if ([i] % 2 !== 0) {
+//         console.log( [i])
+//     }
+// }
+
+// for (let i = 0; i < 100; i++) {
+//     if ([i] % 2 !== 0) {
+//         document.write(`<div>${i}</div>`)
+//     }
+//
+// }
 
 
 
