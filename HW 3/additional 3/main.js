77b1de5b07d07fb87824471c99e323cc0e3d0924
Koval89 <alexandrgalaxy4@gmail.com>
2024-08-28 15:21:02 +0300

    HW 3 additional 3

--- HW 3/additional 3/main.js
+++ HW 3/additional 3/main.js
@@ -38,29 +38,113 @@
 // d. Заповнити масив 20ма рандомними чисалами в діапазоні від 8 до 732 (Google: Generate random number JS)
 
 // debugger
-let number = 20
-let arr = []
 
-for (number; number >= 0; number--) {
-    if (Math.round(Math.random() * 1000) >= 8 && Math.round(Math.random() * 1000) <= 732) {
-        arr.push(number)
-    }
-}
-
-console.log(arr)
+// let number = 20
+// let arr = []
+//
+// for (number; number >= 0; number--) {
+//     if (Math.floor(Math.random() * 1000) >= 8 && Math.floor(Math.random() * 1000) <= 732) {
+//         arr.push(number)
+//     }
+// }
+// console.log(arr)
 
 
 // 2. Вивести за допомогою console.log кожен третій елемен
+
+// debugger
+// let x = [ 1, 2, 3, 5, 7, 9, 56, 8, 67 ]
+// for (let j = 2; j < x.length; j += 3) {
+//     console.log(x[j])
+// }
+
+
 // 3. Вивести за допомогою console.log кожен третій елемен тільки якщо цей елемент є парним.
+
+// let y = [ 1, 2, 4, 5, 7, 8, 56, 8, 67 ]
+// for (let j = 2; j < y.length; j += 3) {
+//     if (y[j] % 2 === 0) {
+//         console.log(y[j])
+//     }
+//
+// }
+
 // 4. Вивести за допомогою console.log кожен третій елемен тільки якщо цей елемент є парним та записати їх в новий масив
+
+// let q = [ 1, 2, 4, 5, 7, 8, 56, 8, 67 ]
+// let newArr = []
+// for (let j = 2; j < q.length; j += 3) {
+//     if (q[j] % 2 === 0) {
+//         newArr.push(q[j])
+//     }
+//
+// }
+// console.log(newArr)
+
+
 // 5. Вивести кожен елемент масиву, сусід справа якого є парним
 // EXAMPLE: [ 1, 2, 3, 5, 7, 9, 56, 8, 67 ] -> Має бути виведено 1, 9, 56
+
+
+
+// let arr = [1, 2, 3, 5, 7, 9, 56, 8, 67]
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 === 0) {
+//         console.log(arr[i-1])
+//     }
+// }
+
+// if (number[i] %2 === 0){
+//     console.log (number[i-1])}
 // 6. Є масив з числами [100,250,50,168,120,345,188], Які характеризують вартість окремої покупки. Обрахувати середній чек.
+
+// let arr = [100, 250, 50, 168, 120, 345, 188]
+//
+// function check(arr){
+// let sum = 0
+//     for (let i = 0; i < arr.length; i++) {
+//         const num = arr[i];
+//         sum = sum + num
+//     }
+//     return sum/arr.length
+// }
+//
+// console.log(check(arr))
+
 //
 // 7. Створити масив з рандомними значеннями, помножити всі його елементи на 5 та перемістити їх в інший масив.
+
+// let number = 0
+// let arr = []
+// for (number; number < 10; number++) {
+//    arr.push(Math.round(Math.random()*10))
+// }
+// console.log(arr)
+//
+//
+// let arr1 = []
+// for (let i = 0; i < arr.length; i++) {
+//     const num = arr[i];
+//     arr1.push(num*5)
+// }
+// console.log(arr1)
+
 // 8. Створити масив з будь якими значеннями (стрінги, числа, і тд...). пройтись по ньому, і якщо елемент є числом - додати його в інший масив.
 //
 //
+
+// let array = [11,22,true,'apple','samsung',false,55]
+// let num = []
+//
+// for (let i = 0; i < array.length; i++) {
+//     if (typeof array[i] === "number"  ){
+//         num.push(array[i])
+//     }
+//
+// }
+//
+// console.log(num)
+
 //
 // - Дано 2 масиви з рівною кількістю об'єктів.
 // Масиви:
